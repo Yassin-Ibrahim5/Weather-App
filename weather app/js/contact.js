04886276221a29c09c6f5c33924527c84c70ec4d
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-07-01 13:53:44 +0300

    latest updates

--- weather app/js/contact.js
+++ weather app/js/contact.js
@@ -1,7 +1,32 @@
 document.getElementById("mode").addEventListener("click", () => {
     document.body.dataset.theme = document.body.dataset.theme === 'light' ? 'dark' : 'light';
-    document.getElementById("hours-carousel").classList.toggle("carousel-dark");
     console.log(document.body.dataset.theme);
     document.getElementById("navbar-icon").src = document.body.dataset.theme === 'light' ? "images/favicon.png" : "images/favicon-dark.png";
 });
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    function setActiveLink() {
+        let currentHash = window.location.hash || '#contact';
+        navLinks.forEach((link) => {
+            link.classList.remove('active');
+
+            if (link.getAttribute("href") === currentHash) {
+                link.classList.add('active');
+            }
+        })
+    }
+
+    setActiveLink();
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", function () {
+            navLinks.forEach((l) => l.classList.remove('active'));
+            this.classList.add('active');
+        })
+    })
+
+    window.addEventListener("hashchange", setActiveLink)
+});
